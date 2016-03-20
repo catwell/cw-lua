b67a4ebf52518e701165d4a15735fd5226bbdedd
Pierre Chapuis <catwell-github@catwell.info>
2016-03-20 18:03:04 +0100

    etpan: complete imap-sample implementation

--- etpan/etpan.c
+++ etpan/etpan.c
@@ -7,9 +7,19 @@
 
 #include <libetpan/libetpan.h>
 
+static bool _imap_error(int r)
+{
+    return (
+        r != MAILIMAP_NO_ERROR &&
+        r != MAILIMAP_NO_ERROR_AUTHENTICATED &&
+        r != MAILIMAP_NO_ERROR_NON_AUTHENTICATED
+    );
+}
+
 static int ll_mailimap_new(lua_State *L)
 {
     struct mailimap *p = mailimap_new(0, NULL);
+    if (!p) return 0;
     lua_pushlightuserdata(L, p);
     return 1;
 }
@@ -67,8 +77,245 @@ static int ll_mailimap_select(lua_State *L)
     return 1;
 }
 
+static int ll_mailimap_fetch(lua_State *L)
+{
+    luaL_checktype(L, 1, LUA_TLIGHTUSERDATA);
+    luaL_checktype(L, 2, LUA_TLIGHTUSERDATA);
+    luaL_checktype(L, 3, LUA_TLIGHTUSERDATA);
+    struct mailimap *p1 = lua_touserdata(L, 1);
+    struct mailimap_set *p2 = lua_touserdata(L, 2);
+    struct mailimap_fetch_type *p3 = lua_touserdata(L, 3);
+    clist *fetch_result;
+    int r = mailimap_fetch(p1, p2, p3, &fetch_result);
+    lua_pushinteger(L, r);
+    if (_imap_error(r)) {
+        return 1;
+    }
+    else {
+        lua_pushlightuserdata(L, fetch_result);
+        return 2;
+    }
+}
+
+static int ll_mailimap_uid_fetch(lua_State *L)
+{
+    luaL_checktype(L, 1, LUA_TLIGHTUSERDATA);
+    luaL_checktype(L, 2, LUA_TLIGHTUSERDATA);
+    luaL_checktype(L, 3, LUA_TLIGHTUSERDATA);
+    struct mailimap *p1 = lua_touserdata(L, 1);
+    struct mailimap_set *p2 = lua_touserdata(L, 2);
+    struct mailimap_fetch_type *p3 = lua_touserdata(L, 3);
+    clist *fetch_result;
+    int r = mailimap_uid_fetch(p1, p2, p3, &fetch_result);
+    lua_pushinteger(L, r);
+    if (_imap_error(r)) {
+        return 1;
+    }
+    else {
+        lua_pushlightuserdata(L, fetch_result);
+        return 2;
+    }
+}
+
+static int ll_mailimap_fetch_list_free(lua_State *L)
+{
+    luaL_checktype(L, 1, LUA_TLIGHTUSERDATA);
+    clist *p = lua_touserdata(L, 1);
+    mailimap_fetch_list_free(p);
+    return 0;
+}
+
+static int ll_mailimap_set_new_interval(lua_State *L)
+{
+    luaL_checktype(L, 1, LUA_TNUMBER);
+    luaL_checktype(L, 2, LUA_TNUMBER);
+    uint32_t first = lua_tointeger(L, 1);
+    uint32_t last = lua_tointeger(L, 2);
+    struct mailimap_set *p = mailimap_set_new_interval(first, last);
+    if (!p) return 0;
+    lua_pushlightuserdata(L, p);
+    return 1;
+}
+
+static int ll_mailimap_set_new_single(lua_State *L)
+{
+    luaL_checktype(L, 1, LUA_TNUMBER);
+    uint32_t indx = lua_tointeger(L, 1);
+    struct mailimap_set *p = mailimap_set_new_single(indx);
+    if (!p) return 0;
+    lua_pushlightuserdata(L, p);
+    return 1;
+}
+
+static int ll_mailimap_fetch_type_new_fetch_att_list_empty(lua_State *L)
+{
+    struct mailimap_fetch_type *p =
+        mailimap_fetch_type_new_fetch_att_list_empty();
+    if (!p) return 0;
+    lua_pushlightuserdata(L, p);
+    return 1;
+}
+
+static int ll_mailimap_fetch_type_new_fetch_att_list_add(lua_State *L)
+{
+    luaL_checktype(L, 1, LUA_TLIGHTUSERDATA);
+    luaL_checktype(L, 2, LUA_TLIGHTUSERDATA);
+    struct mailimap_fetch_type *p1 = lua_touserdata(L, 1);
+    struct mailimap_fetch_att *p2 = lua_touserdata(L, 2);
+    int r = mailimap_fetch_type_new_fetch_att_list_add(p1, p2);
+    lua_pushinteger(L, r);
+    return 1;
+}
+
+static int ll_mailimap_fetch_att_new_uid(lua_State *L)
+{
+    struct mailimap_fetch_att *p = mailimap_fetch_att_new_uid();
+    if (!p) return 0;
+    lua_pushlightuserdata(L, p);
+    return 1;
+}
+
+static int ll_mailimap_fetch_att_new_body_peek_section(lua_State *L)
+{
+    luaL_checktype(L, 1, LUA_TLIGHTUSERDATA);
+    struct mailimap_section *section = lua_touserdata(L, 1);
+    struct mailimap_fetch_att *p =
+        mailimap_fetch_att_new_body_peek_section(section);
+    if (!p) return 0;
+    lua_pushlightuserdata(L, p);
+    return 1;
+}
+
+static int ll_clist_begin(lua_State *L)
+{
+    luaL_checktype(L, 1, LUA_TLIGHTUSERDATA);
+    clist *p = lua_touserdata(L, 1);
+    clistiter *rp = clist_begin(p);
+    if (!rp) return 0;
+    lua_pushlightuserdata(L, rp);
+    return 1;
+}
+
+static int ll_clist_end(lua_State *L)
+{
+    luaL_checktype(L, 1, LUA_TLIGHTUSERDATA);
+    clist *p = lua_touserdata(L, 1);
+    clistiter *rp = clist_end(p);
+    if (!rp) return 0;
+    lua_pushlightuserdata(L, rp);
+    return 1;
+}
+
+static int ll_clist_next(lua_State *L)
+{
+    luaL_checktype(L, 1, LUA_TLIGHTUSERDATA);
+    clistiter *p = lua_touserdata(L, 1);
+    clistiter *rp = clist_next(p);
+    if (!rp) return 0;
+    lua_pushlightuserdata(L, rp);
+    return 1;
+}
+
+static int ll_clist_previous(lua_State *L)
+{
+    luaL_checktype(L, 1, LUA_TLIGHTUSERDATA);
+    clistiter *p = lua_touserdata(L, 1);
+    clistiter *rp = clist_previous(p);
+    if (!rp) return 0;
+    lua_pushlightuserdata(L, rp);
+    return 1;
+}
+
+static int ll_clist_content(lua_State *L)
+{
+    luaL_checktype(L, 1, LUA_TLIGHTUSERDATA);
+    clistiter *p = lua_touserdata(L, 1);
+    void *rp = clist_content(p);
+    if (!rp) return 0;
+    lua_pushlightuserdata(L, rp);
+    return 1;
+}
+
+/* from imap-sample.c */
+static uint32_t _get_uid(struct mailimap_msg_att * msg_att)
+{
+    clistiter * cur;
+
+  /* iterate on each result of one given message */
+    for(cur = clist_begin(msg_att->att_list) ; cur != NULL ; cur = clist_next(cur)) {
+        struct mailimap_msg_att_item * item;
+
+        item = clist_content(cur);
+        if (item->att_type != MAILIMAP_MSG_ATT_ITEM_STATIC) {
+            continue;
+        }
+
+        if (item->att_data.att_static->att_type != MAILIMAP_MSG_ATT_UID) {
+            continue;
+        }
+
+        return item->att_data.att_static->att_data.att_uid;
+    }
+
+    return 0;
+}
+
+static int ll_mailimap_msg_att_get_uid(lua_State *L)
+{
+    luaL_checktype(L, 1, LUA_TLIGHTUSERDATA);
+    struct mailimap_msg_att *msg_att = lua_touserdata(L, 1);
+    uint32_t r = _get_uid(msg_att);
+    lua_pushinteger(L, r);
+    return 1;
+}
+
+/* from imap-sample.c */
+static char * _get_msg_att_msg_content(struct mailimap_msg_att * msg_att, size_t * p_msg_size)
+{
+    clistiter * cur;
+
+  /* iterate on each result of one given message */
+    for(cur = clist_begin(msg_att->att_list) ; cur != NULL ; cur = clist_next(cur)) {
+        struct mailimap_msg_att_item * item;
+
+        item = clist_content(cur);
+        if (item->att_type != MAILIMAP_MSG_ATT_ITEM_STATIC) {
+            continue;
+        }
+
+    if (item->att_data.att_static->att_type != MAILIMAP_MSG_ATT_BODY_SECTION) {
+            continue;
+    }
+
+        * p_msg_size = item->att_data.att_static->att_data.att_body_section->sec_length;
+        return item->att_data.att_static->att_data.att_body_section->sec_body_part;
+    }
+
+    return NULL;
+}
+
+static int ll_mailimap_msg_att_get_msg_content(lua_State *L)
+{
+    luaL_checktype(L, 1, LUA_TLIGHTUSERDATA);
+    struct mailimap_msg_att *msg_att = lua_touserdata(L, 1);
+    size_t sz;
+    char* p = _get_msg_att_msg_content(msg_att, &sz);
+    if (!p) return 0;
+    lua_pushlstring(L, p, sz);
+    return 1;
+}
+
+static int ll_mailimap_section_new(lua_State *L)
+{
+    struct mailimap_section *p = mailimap_section_new(NULL);
+    if (!p) return 0;
+    lua_pushlightuserdata(L, p);
+    return 1;
+}
+
 int luaopen_etpan_ll(lua_State *L)
 {
+
     struct luaL_Reg imap[] = {
         { "new", ll_mailimap_new },
         { "free", ll_mailimap_free },
@@ -76,12 +323,74 @@ int luaopen_etpan_ll(lua_State *L)
         { "login", ll_mailimap_login },
         { "logout", ll_mailimap_logout },
         { "select", ll_mailimap_select },
+        { "fetch", ll_mailimap_fetch },
+        { "uid_fetch", ll_mailimap_uid_fetch },
+        { "fetch_list_free", ll_mailimap_fetch_list_free },
+        { NULL, NULL }
+    };
+
+    struct luaL_Reg imap_set[] = {
+        { "new_interval", ll_mailimap_set_new_interval },
+        { "new_single", ll_mailimap_set_new_single },
+        { NULL, NULL }
+    };
+
+    struct luaL_Reg imap_fetch_type[] = {
+        {
+            "new_fetch_att_list_empty",
+            ll_mailimap_fetch_type_new_fetch_att_list_empty
+        },
+        {
+            "new_fetch_att_list_add",
+            ll_mailimap_fetch_type_new_fetch_att_list_add
+        },
+        { NULL, NULL }
+    };
+
+    struct luaL_Reg imap_fetch_att[] = {
+        { "new_uid", ll_mailimap_fetch_att_new_uid },
+        {
+            "new_body_peek_section",
+            ll_mailimap_fetch_att_new_body_peek_section
+        },
+        { NULL, NULL }
+    };
+
+    struct luaL_Reg clist[] = {
+        { "begin", ll_clist_begin },
+        { "end", ll_clist_end },
+        { "next", ll_clist_next },
+        { "previous", ll_clist_previous },
+        { "content", ll_clist_content },
+        { NULL, NULL }
+    };
+
+    struct luaL_Reg msg_att[] = {
+        { "get_uid", ll_mailimap_msg_att_get_uid },
+        { "get_msg_content", ll_mailimap_msg_att_get_msg_content },
+        { NULL, NULL }
+    };
+
+    struct luaL_Reg section[] = {
+        { "new", ll_mailimap_section_new },
         { NULL, NULL }
     };
 
     lua_newtable(L); /* ll */
-    luaL_newlib(L, imap);
-    lua_setfield (L, -2, "imap");
+        luaL_newlib(L, imap);
+            luaL_newlib(L, imap_set);
+            lua_setfield (L, -2, "set");
+            luaL_newlib(L, imap_fetch_att);
+            lua_setfield (L, -2, "fetch_att");
+            luaL_newlib(L, imap_fetch_type);
+            lua_setfield (L, -2, "fetch_type");
+        lua_setfield (L, -2, "imap");
+        luaL_newlib(L, clist);
+        lua_setfield (L, -2, "clist");
+        luaL_newlib(L, msg_att);
+        lua_setfield (L, -2, "msg_att");
+        luaL_newlib(L, section);
+        lua_setfield (L, -2, "section");
 
     return 1;
 }
--- etpan/tests/imap-sample.lua
+++ etpan/tests/imap-sample.lua
@@ -1,3 +1,4 @@
+local lfs = require "lfs"
 local etpan = require "etpan"
 local fmt = string.format
 
@@ -7,19 +8,88 @@ local check_error = function(c)
     end
 end
 
-local fetch_messages = function(imap)
-    print("TODO")
+local get_msg_content = function(fetch_result)
+    local cur = etpan.ll.clist.begin(fetch_result)
+    while cur do
+        local msg_att = etpan.ll.clist.content(cur)
+        local msg_content = etpan.ll.msg_att.get_msg_content(msg_att)
+        if msg_content then return msg_content end
+        cur = etpan.ll.clist.next(cur)
+    end
 end
 
-assert(arg[1] and arg[2], fmt("USAGE: %s userid password", arg[0]))
+local fetch_msg = function(imap, uid)
 
-local imap = etpan.ll.imap.new()
+    local filename = fmt("download/%d.eml", uid)
+    if lfs.attributes(filename) then
+        print(fmt("%d is already fetched", uid))
+        return
+    end
 
-check_error(etpan.ll.imap.ssl_connect(imap, "imap.gmail.com", 993))
-check_error(etpan.ll.imap.login(imap, arg[1], arg[2]))
-check_error(etpan.ll.imap.select(imap, "INBOX"))
+    local set = etpan.ll.imap.set.new_single(uid)
+    local fetch_type = etpan.ll.imap.fetch_type.new_fetch_att_list_empty()
+    local section = etpan.ll.section.new()
+    local fetch_att = etpan.ll.imap.fetch_att.new_body_peek_section(section)
+    etpan.ll.imap.fetch_type.new_fetch_att_list_add(fetch_type, fetch_att)
 
-fetch_messages(imap)
+    local r, fetch_result = etpan.ll.imap.uid_fetch(imap, set, fetch_type)
+    check_error(r)
+    print(fmt("fetch %d", uid))
 
-etpan.ll.imap.logout(imap)
-etpan.ll.imap.free(imap)
+    local msg_content = get_msg_content(fetch_result)
+    if not msg_content then
+        io.stderr:write("no content\n")
+        etpan.ll.imap.fetch_list_free(fetch_result)
+        return
+    end
+
+    local f = io.open(filename, "w")
+    if not f then
+        io.stderr:write("could not write\n")
+        etpan.ll.imap.fetch_list_free(fetch_result)
+        return
+    end
+    f:write(msg_content)
+    f:close()
+
+    print(fmt("%d has been fetched", uid))
+    etpan.ll.imap.fetch_list_free(fetch_result)
+end
+
+local fetch_messages = function(imap)
+    local set = etpan.ll.imap.set.new_interval(1, 0)
+    local fetch_type = etpan.ll.imap.fetch_type.new_fetch_att_list_empty()
+    local fetch_att = etpan.ll.imap.fetch_att.new_uid()
+    etpan.ll.imap.fetch_type.new_fetch_att_list_add(fetch_type, fetch_att)
+    local r, fetch_result = etpan.ll.imap.fetch(imap, set, fetch_type)
+    check_error(r)
+    local cur = etpan.ll.clist.begin(fetch_result)
+    while cur do
+        local msg_att = etpan.ll.clist.content(cur)
+        local uid = etpan.ll.msg_att.get_uid(msg_att)
+        if uid ~= 0 then
+            fetch_msg(imap, uid)
+        end
+        cur = etpan.ll.clist.next(cur)
+    end
+    etpan.ll.imap.fetch_list_free(fetch_result)
+end
+
+local main = function(arg)
+    assert(arg[1] and arg[2], fmt("USAGE: %s userid password", arg[0]))
+
+    lfs.mkdir("download")
+
+    local imap = etpan.ll.imap.new()
+
+    check_error(etpan.ll.imap.ssl_connect(imap, "imap.gmail.com", 993))
+    check_error(etpan.ll.imap.login(imap, arg[1], arg[2]))
+    check_error(etpan.ll.imap.select(imap, "INBOX"))
+
+    fetch_messages(imap)
+
+    etpan.ll.imap.logout(imap)
+    etpan.ll.imap.free(imap)
+end
+
+main(arg)
